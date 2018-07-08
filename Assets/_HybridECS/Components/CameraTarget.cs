d7e1d76411b8f56cdb5fa2068cfec333cc6cb34c
bill <bill@williamwheatley.com>
2018-07-08 13:40:04 -0400

    Basic Camera following working using ECS hybrid systems.

---
+++ Assets/_HybridECS/Components/CameraTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hybrid.Components
+{
+    public class CameraTarget : MonoBehaviour {
+        public Transform PlayerTransform;
+        public Transform CameraTransform;
+        public Vector3 offset;
+
+        //We need to make sure that the camera is always using the right offset
+        void Awake()
+        {
+            offset = CameraTransform.position - PlayerTransform.position;      
+        }
+    }
+}
---
+++ Assets/_HybridECS/Systems/CameraTrackSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Hybrid.Components;
+using UnityEngine;
+using Unity.Entities;
+
+namespace Hybrid.Systems
+{
+    public class CameraTrackSystem : ComponentSystem
+    {
+        private struct Group {
+            public CameraTarget CameraTarget;
+        }
+
+
+        protected override void OnUpdate() {
+            foreach (var entity in GetEntities<Group>()) {
+                entity.CameraTarget.CameraTransform.position =
+                    entity.CameraTarget.PlayerTransform.position + entity.CameraTarget.offset;
+            }
+        }
+    }
+
+}
